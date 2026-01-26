27f16e44b2ffdfd8093e59e1477fe1d8bbf045d0
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-01-26 13:05:08 +0530

    Styling Component: Using Vanilla CSS

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "Los Angeles", "San Francisco"];
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading="Miami"
+        onSelectItem={() => {}}
+      ></ListGroup>
     </div>
   );
 }
