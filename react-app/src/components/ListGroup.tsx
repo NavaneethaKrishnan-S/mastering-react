179f3cd6c2f6278e2676978b8c774506235aefad
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-01-26 09:03:34 +0530

    Conditional Rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "London", "Paris", "Tokyo"];
+  let items = ["New York", "San Francisco", "London", "Paris", "Tokyo"];
+  // items = [];
 
   return (
     <>
       <h1>List</h1>
+      {/* If the items array is empty, React conditionally renders the text “No item found”. */}
+      {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
