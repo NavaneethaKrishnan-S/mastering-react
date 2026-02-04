0d061d2d4d7cb7b8afdecc94ab21994ce98b33d4
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-04 15:55:18 +0530

    Styling Component: Exercise using CSS Modules

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,14 @@
 import ListGroup from "./components/ListGroup";
 import "./App.css";
 import { BsFillCalendarFill } from "react-icons/bs";
+import Button from "./components/Button/Button";
 
 function App() {
   let items = ["New York", "Los Angeles", "San Francisco"];
 
   return (
     <div>
-      <BsFillCalendarFill color="red" size={40} />
-      <ListGroup
-        items={items}
-        heading="Miami"
-        onSelectItem={() => {}}
-      ></ListGroup>
+      <Button onClick={() => {}}>My Button </Button>
     </div>
   );
 }
