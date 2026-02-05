7476e9b58e8e1fdda3c174946531f5168869adc0
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 14:22:26 +0530

    Building Forms: Building a Form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,11 @@
 import "./App.css";
 import ExapandableText from "./components/ExapandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExapandableText>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla earum
-        quae ab sunt ullam ea, quidem dolore tempora dolores mollitia modi
-        voluptatum voluptatibus magnam repellendus animi quaerat odio vel
-        consectetur optio, corporis quam culpa explicabo commodi totam. Tempora
-        mollitia reiciendis facilis obcaecati fugit excepturi maiores, ab,
-        dignissimos accusantium, veniam nam! Consequuntur, dolore voluptas optio
-        dolorem nam nobis sapiente quo quasi minima molestias in eius! Quas
-        deserunt accusantium, ipsam qui ab quis distinctio! Iusto atque rem
-        aspernatur ratione totam, cupiditate unde tempora rerum reprehenderit
-        quia aliquam earum facilis perferendis dicta, blanditiis magnam? Numquam
-        nisi eaque fugiat dolorem quaerat dolores vitae deleniti.
-      </ExapandableText>
+      <Form />
     </div>
   );
 }
