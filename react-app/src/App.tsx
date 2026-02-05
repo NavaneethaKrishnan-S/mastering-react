40792ff619cc32e016780cc53bc721b71c7130be
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 10:58:44 +0530

    Managing Component State: Updating Nested Objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,30 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
   });
 
   const handleClick = () => {
-    // const newDrink = {
-    //   title: drink.title,
-    //   price: 6,
-    // };
-    // setDrink(newDrink);
-
-    // const newDrink = {
-    //   ...drink,
-    //   price: 6,
-    // };
-    // setDrink(newDrink);
-
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
   };
 
   return (
     <div>
-      {drink.price}
+      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
