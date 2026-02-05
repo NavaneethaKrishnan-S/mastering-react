fc7ae639c4b9565d6949f1f3108856b06b20a458
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 09:07:53 +0530

    Managing Component State: Exercise 3

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-    console.log(pizza.toppings);
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item,
+      ),
+    });
   };
 
   return (
