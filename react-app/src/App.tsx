af688e2d2dd7c73e3b7e8483954ffe8b2d4f5ae8
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 10:51:07 +0530

    Managing Component State: Updating Objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import "./App.css";
-import Message from "./Message";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    // const newDrink = {
+    //   title: drink.title,
+    //   price: 6,
+    // };
+    // setDrink(newDrink);
+
+    // const newDrink = {
+    //   ...drink,
+    //   price: 6,
+    // };
+    // setDrink(newDrink);
+
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
