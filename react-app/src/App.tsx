83102132c3bb880095eeed008f16cb6464ffded9
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 10:29:51 +0530

    Managing Component State: Understanding Strict Mode

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,27 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item,
-      ),
-    });
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Click me</button>
+      <Message />
     </div>
   );
 }
--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,9 +1,9 @@
-function Message() {
-    // JSX: JavaScript XML
-    const name = '';
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World!</h1>
-}
+let count = 0;
+
+const Message = () => {
+  console.log("Message called", count);
+  count = count + 1;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
