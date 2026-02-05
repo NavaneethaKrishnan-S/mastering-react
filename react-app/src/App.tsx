fb1d82237247c265b06be8494dedc2c3f45f6532
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-02-05 13:01:09 +0530

    Managing Component State: Exercise - Building Expandable Text

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
-import produce from "immer";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExapandableText from "./components/ExapandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExapandableText>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla earum
+        quae ab sunt ullam ea, quidem dolore tempora dolores mollitia modi
+        voluptatum voluptatibus magnam repellendus animi quaerat odio vel
+        consectetur optio, corporis quam culpa explicabo commodi totam. Tempora
+        mollitia reiciendis facilis obcaecati fugit excepturi maiores, ab,
+        dignissimos accusantium, veniam nam! Consequuntur, dolore voluptas optio
+        dolorem nam nobis sapiente quo quasi minima molestias in eius! Quas
+        deserunt accusantium, ipsam qui ab quis distinctio! Iusto atque rem
+        aspernatur ratione totam, cupiditate unde tempora rerum reprehenderit
+        quia aliquam earum facilis perferendis dicta, blanditiis magnam? Numquam
+        nisi eaque fugiat dolorem quaerat dolores vitae deleniti.
+      </ExapandableText>
     </div>
   );
 }
